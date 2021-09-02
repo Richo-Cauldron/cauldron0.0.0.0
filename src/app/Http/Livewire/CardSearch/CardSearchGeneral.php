1f6ef2b2bbce223472b60c060d68ff04b865f2b7
Orchard <rjbatson1@gmail.com>
2021-09-02 22:15:08 +1000

    general card search initial setup

--- src/app/Http/Livewire/CardSearch/CardSearchGeneral.php
+++ src/app/Http/Livewire/CardSearch/CardSearchGeneral.php
@@ -2,37 +2,16 @@
 
 namespace App\Http\Livewire\CardSearch;
 
+
 use Livewire\Component;
-use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Illuminate\Pagination\Paginator;
+use App\Http\Livewire\Traits\CardSearchGeneralTrait;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Pagination\LengthAwarePaginator;
-// use Livewire\WithPagination;
+
 
 class CardSearchGeneral extends Component
 {   
-    // use WithPagination;
 
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public $cardSearchValue;
-
-    public $cardSearchResults = [];
-
-    public $cardSearchErrorComment;
-
-    public $totalCardsReturned;
-
-    public $responseMessage;
-
-    public $undefined = 'Sorry ... Data not available.';
-
-    public $pageSize = 20;
-
-    // protected $paginationData;
-
-    public $resultCollection;
+    use CardSearchGeneralTrait;
 
     // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
@@ -45,86 +24,6 @@ class CardSearchGeneral extends Component
     }
 
     // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public function checkForCardSearchValueErrorException(): void
-    {
-        if (strlen($this->cardSearchValue) >= 2 ) {
-
-            try {
-                $this->cardSearchResults = $this->httpAPIRequestUrlget->json()['data'];
-            } catch (\Exception $e) {
-                $e = 'Unable to match "' . $this->cardSearchValue . '"';
-                $this->cardSearchErrorComment = $e;
-                // $this->totalCardsReturned = '';
-            }
-        }
-        $this->totalCardsReturned = Count($this->cardSearchResults);
-        // dump($this->cardSearchResults);
-    }
-
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public function showInputMessage()
-    {
-        // if (strlen($this->cardSearchValue) < 2) {
-        //     $this->responseMessage = '';
-        // }
-        
-        if ($this->cardSearchResults) {
-            $this->responseMessage = 'Total Cards: ' . $this->totalCardsReturned .' containing ' . $this->cardSearchValue;
-            // $this->cardSearchValue ='';
-        } elseif ($this->cardSearchErrorComment) {
-            // $this->responseMessage = '';
-            $this->responseMessage = $this->cardSearchErrorComment; 
-            // $this->cardSearchValue =''; 
-            $this->hydrate();
-        }
-    }
-
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public function cardSearchAPIurl()
-    {
-       $this->httpAPIRequestUrlget = Http::get('https://api.scryfall.com/cards/search?q=' . $this->cardSearchValue);
-    }
-
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public function checkSearchInputEmpty()
-    {
-        if (strlen($this->cardSearchValue) < 1 ) {
-            $this->responseMessage ='You didn‘t enter anything to search for';
-        }
-    }
-
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    public function cardSearchPagination()
-    {
-        // $total=count($this->cardSearchResults);
-        // $per_page = 20;
-        // $current_page = 1;
-
-        // $starting_point = ($current_page * $per_page) - $per_page;
-
-        // $array = array_slice($this->cardSearchResults, $starting_point, $per_page, true);
-
-        // $this->paginationData= new LengthAwarePaginator($array, $total, $per_page, $current_page, [
-        //     'path' => $request->url(),
-        //     'query' => $request->query(),
-        // ]);
-
-        // dump($this->paginationData);
-    }
-
-    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-    // public function paginate($items, $perPage = 20, $page = null, $options = [])
-    // {
-    //     $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-    //     $items = $items instanceof Collection ? $items : Collection::make($items);
-    //     return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
-    // }
 
     // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
@@ -142,10 +41,10 @@ class CardSearchGeneral extends Component
             'cardSearchResults' => $this->cardSearchResults,
             'responseMessage' => $this->responseMessage,
             'totalCardsReturned' => $this->totalCardsReturned,
-            'undefined' => $this->undefined,
-            // 'paginationData' => $this->paginationData,
-            
+            'undefined' => $this->undefined,   
         ]);
     }
+
+    // +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
             
 }
